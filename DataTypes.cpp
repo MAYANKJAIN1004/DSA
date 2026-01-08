317d464ff5de5311f03523eb2df7ee70614e8bfb
Mayank Jain <mayank.jain1004@gmail.com>
2026-01-08 12:06:58 +0530

    DataTypes

---
+++ DataTypes.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Data Types in C++:" << endl;
+    cout << "Int : " << sizeof(int) << endl;
+    cout << "Float : " << sizeof(float) << endl;
+    cout << "Double : " << sizeof(double) << endl;
+    cout << "Char : " << sizeof(char) << endl;
+    cout << "Bool : " << sizeof(bool) << endl;
+    cout << "Long : " << sizeof(long) << endl;
+    cout << "Long Long : " << sizeof(long long) << endl;
+    cout << "Short : " << sizeof(short) << endl;
+    cout << "long int : " << sizeof(long int) << endl;
+    cout << "unsigned int : " << sizeof(unsigned int) << endl;
+    cout << "Short int : " << sizeof(short int) << endl;
+    
+    return 0;
+}
